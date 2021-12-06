e951cc97336f3203912354a7180c3b57fdcd23e9
shabloel <shabloel@outlook.com>
2021-12-06 23:30:59 +0100

    new code

--- src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
@@ -1,14 +1,34 @@
 package com.appsdeveloperblog.ws.clients.photoappwebclient.controllers;
 
+import com.appsdeveloperblog.ws.clients.photoappwebclient.response.AlbumRest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
+//its a client webapplication therefore no @Restcontroller annotation
 @Controller
 public class AlbumsController {
 
     @GetMapping("/albums")
     public String getAlbums(Model model){
+
+        AlbumRest album = AlbumRest.builder()
+                .albumId("albumOne")
+                .albumTitle("Album one title")
+                .albumUrl("http://localhost:8082/albums/1")
+                .build();
+
+        AlbumRest album2 = AlbumRest.builder()
+                .albumId("albumTwo")
+                .albumTitle("Album two title")
+                .albumUrl("http://localhost:8082/albums/2")
+                .build();
+
+        model.addAttribute("albums", Arrays.asList(album, album2));
+
         return "albums";
     }
 }
